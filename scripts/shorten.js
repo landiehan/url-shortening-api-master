ba3e58fed3991b174c1e526c87e7306f97fa20d3
landiehan <67254560+landiehan@users.noreply.github.com>
2020-10-07 18:47:35 +0800

    shorten & copy & mobile layout - OK!!

---
+++ scripts/shorten.js
@@ -0,0 +1,67 @@
+const sectionResult = document.querySelector('.main-shorten');
+const form = document.querySelector('.main-shorten-form');
+const inputURL = document.querySelector('.main-shorten-form input');
+const btnsCopy = document.querySelectorAll('.main-shorten-result button');
+
+const baseURL = 'https://rel.ink/api/links/';
+
+form.addEventListener('submit', shortenURL);
+
+function shortenURL(e) {
+    e.preventDefault();
+
+    formatValidation(inputURL);
+
+    fetch(baseURL,
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            url: `${inputURL.value}`
+        })
+    })
+    .then(res => {
+        return res.json();
+    })
+    .then(resJson => {
+        displayResults(resJson.hashid);
+    });
+}
+
+function formatValidation(inputURL) {
+    if (!inputURL.value) {
+        inputURL.parentNode.classList.add('error-input__empty');
+        return;
+    } else {
+        inputURL.parentNode.classList.remove('error-input__empty');
+    };
+}
+
+function displayResults(hashid) {
+    let resultCard = document.createElement('section');
+    resultCard.classList.add('main-shorten-result');
+    resultCard.innerHTML = `
+    <div class="main-shorten-result-origin">
+      <p>${inputURL.value}</p>
+    </div>
+    <div class="main-shorten-result-final">
+      <a href='https://rel.ink/${hashid}'>
+        https://rel.ink/${hashid}
+      </a>
+      <button>Copy</button>
+    </div>
+    `;
+    sectionResult.appendChild(resultCard);
+    
+    let btn = resultCard.querySelector('button');
+    btn.addEventListener('click', function copyURL() {
+        let resultURL = `https://rel.ink/${hashid}`;
+        navigator.clipboard.writeText(resultURL)
+        .then(() => {
+            btn.innerText = 'Copied!';
+            btn.style.backgroundColor = 'hsl(257, 27%, 26%)';             
+        })
+    });
+}
